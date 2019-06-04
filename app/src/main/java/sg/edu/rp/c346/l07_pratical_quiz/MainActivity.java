f71fe90592554d027991027dd2adcc0e3c11f5f1
17010144 <17010144@myrp.edu.sg>
2019-06-04 16:20:04 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/l07_pratical_quiz/MainActivity.java
@@ -0,0 +1,101 @@
+package sg.edu.rp.c346.l07_pratical_quiz;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvSms;
+    Button btnRetrieve;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvSms = findViewById(R.id.tv);
+        btnRetrieve = findViewById(R.id.btnRetrieve);
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int permissionCheck = PermissionChecker.checkSelfPermission
+                        (MainActivity.this, Manifest.permission.READ_SMS);
+
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_SMS}, 0);
+                    // stops the action from proceeding further as permission not
+                    //  granted yet
+                    return;
+                }
+
+                // Create all messages URI
+                Uri uri = Uri.parse("content://sms");
+                // The columns we want
+                //  date is when the message took place
+                //  address is the number of the other party
+                //  body is the message content
+                //  type 1 is received, type 2 sent
+                String[] reqCols = new String[]{"address", "body"};
+
+                // Get Content Resolver object from which to
+                //  query the content provider
+                ContentResolver cr = getContentResolver();
+                // Fetch SMS Message from Built-in Content Provider
+                // The filter String
+                String filter="address LIKE ? AND body LIKE ?";
+                // The matches for the ?
+
+                String[] filterArgs = {"%55%","%RP"};
+
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+                if (cursor.moveToFirst()) {
+                    do {
+
+                        String address = cursor.getString(0);
+                        String body = cursor.getString(1);
+
+                        smsBody =  address + ": " + body ;
+
+
+                    } while (cursor.moveToNext());
+                }
+                tvSms.setText(smsBody);
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the read SMS
+                    //  as if the btnRetrieve is clicked
+                    btnRetrieve.performClick();
+
+                } else {
+                    // permission denied... notify user
+                    Toast.makeText(MainActivity.this, "Permission not granted",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+}
